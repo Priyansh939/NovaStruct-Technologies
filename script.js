b21e91ec3f2954dcac3a905e81c9c417f710238d
Priyansh Pandya <priyansh.imscit21@gmail.com>
2026-04-29 22:30:32 +0530

    Implement theme toggle and animated counters
    Add theme toggle functionality and counter animations.

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Site loaded');
+// script.js
+
+const toggle = document.getElementById("themeToggle");
+
+toggle.onclick = () => {
+document.body.classList.toggle("dark");
+};
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+let target = +counter.getAttribute("data-target");
+let count = 0;
+
+let update = setInterval(() => {
+count += Math.ceil(target / 80);
+
+if(count >= target){
+count = target;
+clearInterval(update);
+}
+
+counter.innerText = count;
+}, 30);
+});
